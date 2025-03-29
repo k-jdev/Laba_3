5dca395d0182291a7e9e2d77c28a6577f44c9e4f
k-jdev <svv.ooj@gmail.com>
2025-03-29 20:34:12 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("====================================");
+            Console.WriteLine("           ГОЛОВНЕ МЕНЮ           ");
+            Console.WriteLine("====================================");
+            Console.WriteLine("1 - Почати виконувати програму");
+            Console.WriteLine("2 - Завершити програму");
+            Console.Write("Ваш вибір: ");
+
+            string choice = Console.ReadLine();
+            if (choice == "2") break;
+
+            if (choice == "1")
+            {
+                int[] array = GetArrayFromUser();
+                ChooseTask(array);
+            }
+            else
+            {
+                Console.WriteLine("Невірний вибір, спробуйте ще раз.");
+            }
+        }
+    }
+
+}
